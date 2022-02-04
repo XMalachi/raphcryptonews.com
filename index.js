65c93ef8884d4207478e245581a0b527e1b3259c
xmalachi <angelmalachi@gmail.com>
2022-02-04 06:29:49 +0100

    update

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 /*Hamburger Animation*/
-const mainMenu = document.querySelector(`.mainMenu`);
+const nav = document.querySelector(`.nav`);
 const closeMenu = document.querySelector(`.closeMenu`);
 const openMenu = document.querySelector(`.openMenu`);
 // const openservice = document.querySelector(`.#`);
@@ -12,12 +12,12 @@ closeMenu.addEventListener(`click`,close);
 // opencontact.addEventListener(`click`,close);
 
 function show() {
-    mainMenu.style.display = 'flex';
-    mainMenu.style.top = '0';
+    nav.style.display = 'flex';
+    nav.style.top = '0';
 
 }
 function close() {
-    mainMenu.style.top = '-150%';
+    nav.style.top = '-150%';
 
 }
 
